d848663e70a09bd50beb451690f96497007d31e5
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-11 13:44:40 -0300

    feat: update app main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        User user = new User("Farias", "farias@gmail.com", "123456", "123456789", new Module[0]);
+        Module module = new Module("Java", "CS101", "Java is a programming language");
+        user.newModule(module);
+
+        System.out.println(user.getName());
+        System.out.println(user.getModules()[0].name);
+        System.out.println(user.getModules()[0].code);
+        System.out.println(user.getModules().length);
+
+        Module module2 = new Module("C#", "CS102", "C# is a programming language");
+        user.newModule(module2);
+
+        System.out.println(user.getModules()[1].name);
+        System.out.println(user.getModules().length);
 
     }
 }
